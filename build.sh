1578a91ef26d14c1e30043453f7fc1521c0ccb9d
Doug McLain <dougmclain@yahoo.com>
2025-02-10 09:47:08 -0800

    add collected static files

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
 # Run migrations
 python manage.py migrate
